51367e40b7b738919977b766557d919132adc8cb
Sadjed Nemli <sajednemli39@gmail.com>
2024-06-18 22:40:13 +0100

    chore: Add Footer and Logo components for the marketing page

---
+++ app/(marketing)/_components/Logo.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import { Poppins } from 'next/font/google'
+
+import { cn } from '@/lib/utils'
+
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ["400", "600"]
+})
+
+export const Logo = () => {
+  return (
+    <div className='hidden md:flex items-center gap-2'>
+      <Image
+        src='/logo.svg'
+        alt='Sotion'
+        width="40"
+        height="40"
+      />
+      <p className={cn("font-semibold", font.className)}>
+        Sotion
+      </p>
+    </div>
+  )
+}
