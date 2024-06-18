27396ba7824ff7adc00ef69a0f8e0ff7a04cdbee
Sadjed Nemli <sajednemli39@gmail.com>
2024-06-18 22:39:38 +0100

    feat: Add Heading component for the landing page

---
+++ app/(marketing)/_components/Heading.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { ArrowRight } from "lucide-react"
+
+export const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="tet-3xl sm:text-5xl md:text-6xl font-bold">
+        Your Ideas, Documents, and Tasks. Unified. Welcome to <span className="underline">Sotion</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Sotion is the connected workspace where <br /> better, faster work happens.
+      </h3>
+      <Button>
+        Enter Sotion
+        <ArrowRight className="h-4 w-4 ml-2" />
+      </Button>
+    </div>
+  )
+}
