8531b3744a1872284992c3c35ecb5e6fa58ecce3
Sadjed Nemli <sajednemli39@gmail.com>
2024-06-19 23:06:48 +0100

    chore: Update Footer and Logo components for dark mode support

--- app/(marketing)/_components/Logo.tsx
+++ app/(marketing)/_components/Logo.tsx
@@ -16,6 +16,14 @@ export const Logo = () => {
         alt='Sotion'
         width="40"
         height="40"
+        className='dark:hidden'
+      />
+      <Image
+        src='/letter-s.png'
+        alt='Sotion'
+        width="40"
+        height="40"
+        className='hidden dark:block'
       />
       <p className={cn("font-semibold", font.className)}>
         Sotion
