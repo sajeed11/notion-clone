10214ad97c3eace04ebc1466ec076196288c15f6
Sadjed Nemli <sajednemli39@gmail.com>
2024-06-21 20:36:54 +0100

    feat: Add loading state and sign-in buttons to Navbar component

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,11 +1,16 @@
 "use client"
 
+import { useConvexAuth } from "convex/react"
 import { useScrollTop } from "@/hooks/use-scroll-top"
+
 import { cn } from "@/lib/utils"
 import { Logo } from "./Logo"
 import { ModeToggle } from "@/components/mode-toggle"
+import { SignInButton } from "@clerk/clerk-react"
+import { Button } from "@/components/ui/button"
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
   const scrolled = useScrollTop()
 
   return (
@@ -15,6 +20,23 @@ export const Navbar = () => {
     )}>
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && (
+          <p>Loading...</p>
+        )}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">
+                Get Sotion free
+              </Button>
+            </SignInButton>
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
