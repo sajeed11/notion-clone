f83f505d47e6d0955ef0447ccc0cadc73bf99ef6
Sadjed Nemli <sajednemli39@gmail.com>
2024-06-22 18:12:30 +0100

    feat: Update Heading component with loading state and sign-in buttons

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,9 +1,15 @@
 "use client"
 
+import Spinner from "@/components/spinner"
 import { Button } from "@/components/ui/button"
+import { SignInButton } from "@clerk/clerk-react"
+import { useConvexAuth } from "convex/react"
 import { ArrowRight } from "lucide-react"
+import Link from "next/link"
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="tet-3xl sm:text-5xl md:text-6xl font-bold">
@@ -12,10 +18,27 @@ export const Heading = () => {
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Sotion is the connected workspace where <br /> better, faster work happens.
       </h3>
-      <Button>
-        Enter Sotion
-        <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="w-full flex justify-center items-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Sotion
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Sotion free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   )
 }
