064879b59171bac996bb195498ff1c9d51855ce8
Sadjed Nemli <sajednemli39@gmail.com>
2024-06-23 22:12:28 +0100

    feat: Add collapse functionality to Navigation component

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -2,7 +2,7 @@
 
 import { ChevronsLeft, MenuIcon } from "lucide-react"
 import { usePathname } from "next/navigation"
-import React, { ElementRef, useRef, useState } from "react"
+import React, { ElementRef, useEffect, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts"
 
 import { cn } from "@/lib/utils"
@@ -16,6 +16,20 @@ export const Navigation = () => {
   const navbarRef = useRef<ElementRef<"div">>(null)
   const [isResetting, setIsResetting] = useState(false)
   const [isCollapsed, setIsCollapsed] = useState(isMobile)
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse()
+    } else {
+      resetWidth()
+    }
+  }, [isMobile])
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse()
+    }
+  }, [pathname, isMobile])
 
   const handleMouseDown = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -66,6 +80,18 @@ export const Navigation = () => {
     }
   }
 
+  const collapse = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(true)
+      setIsResetting(true)
+
+      sidebarRef.current.style.width = "0"
+      navbarRef.current.style.setProperty("width", "100%")
+      navbarRef.current.style.setProperty("left", "0")
+      setTimeout(() => setIsResetting(false), 300)
+    }
+  }
+
   return (
     <>
       <aside
@@ -77,6 +103,7 @@ export const Navigation = () => {
         )}
       >
         <div
+          onClick={collapse}
           role="button"
           className={cn(
             "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
@@ -106,7 +133,7 @@ export const Navigation = () => {
         )}
       >
         <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />}
+          {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />}
         </nav>
       </div>
     </>
