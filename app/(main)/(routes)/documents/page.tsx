7da2b615e7b85836e9092b003e4b9d403d6182db
Sadjed Nemli <sajednemli39@gmail.com>
2024-06-23 22:26:59 +0100

    feat: Improve DocumentPage with user-specific welcome message and create note button

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,6 +1,30 @@
+"use client"
+
+import Image from "next/image"
+import { useUser } from "@clerk/clerk-react"
+
+import { Button } from "@/components/ui/button"
+import { PlusCircle } from "lucide-react"
+
 const DocumentPage = () => {
+  const { user } = useUser()
+
   return (
-    <div>DocumentPage</div>
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src='/sammy-line-thoughtful-man-with-empty-list.png'
+        height="300"
+        width="300"
+        alt="Empty"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Sotion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
   )
 }
 
