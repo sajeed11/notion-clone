64a8a880e2f532a04055f01107fbd75327aa5381
Sadjed Nemli <sajednemli39@gmail.com>
2024-06-25 19:01:03 +0100

    chore: test the API with the create method

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -2,12 +2,28 @@
 
 import Image from "next/image"
 import { useUser } from "@clerk/clerk-react"
-
-import { Button } from "@/components/ui/button"
 import { PlusCircle } from "lucide-react"
+import { useMutation } from "convex/react"
+import { toast } from "sonner"
+
+import { api } from "@/convex/_generated/api"
+import { Button } from "@/components/ui/button"
 
 const DocumentPage = () => {
   const { user } = useUser()
+
+  // Create a new document
+  const create = useMutation(api.documents.create)
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" })
+
+    toast.promise(promise, {
+      loading: "Creating document...",
+      success: "Document created!",
+      error: "Failed to create document :("
+    })
+  }
 
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -20,7 +36,7 @@ const DocumentPage = () => {
       <h2 className="text-lg font-medium">
         Welcome to {user?.firstName}&apos;s Sotion
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
         Create a note
       </Button>
