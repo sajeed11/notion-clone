d047b5e443a978a9bdc18eda345d2d609c94a45a
Sadjed Nemli <sajednemli39@gmail.com>
2024-06-26 19:07:19 +0100

    chore: edit the kbd tag

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -67,8 +67,8 @@ export const Item = ({
       </span>
       {isSearch && (
         <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span className="text-xs">x
-          </span>k
+          <span className="text-xs">ctrl
+          </span>K
         </kbd>
       )}
     </div>
