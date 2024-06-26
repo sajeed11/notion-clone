80bbc37fa0534ff7e71cd218c9db51ecc910f8cd
Sadjed Nemli <sajednemli39@gmail.com>
2024-06-26 19:54:01 +0100

    feat: add the skeleton component from shadcn/ui and add Item.skeleton to the Item component

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -4,6 +4,7 @@ import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react"
 
 import { Id } from "@/convex/_generated/dataModel"
 import { cn } from "@/lib/utils"
+import { Skeleton } from "@/components/ui/skeleton"
 
 interface ItemProps {
   id?: Id<"documents">,
@@ -71,6 +72,20 @@ export const Item = ({
           </span>K
         </kbd>
       )}
+    </div>
+  )
+}
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div
+      style={{
+        paddingLeft: level ? `${(level * 12) + 25}px` : "12px"
+      }}
+      className="flex gap-x-2 py-[3px]"
+    >
+      <Skeleton className="h-4 w-4" />
+      <Skeleton className="h-4 w-[30%]" />
     </div>
   )
 }
