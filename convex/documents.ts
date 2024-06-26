b3ca6c2b1bc6e2a649a9139a0a427e48eeb1de09
Sadjed Nemli <sajednemli39@gmail.com>
2024-06-26 17:56:35 +0100

    feat: add the get function in convex to get the documents from the db

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,6 +3,20 @@ import { v } from 'convex/values'
 import { mutation, query } from './_generated/server'
 import { Doc, Id } from './_generated/dataModel'
 
+export const get = query({
+  handler: async (ctx) => {
+    const identify = await ctx.auth.getUserIdentity()
+
+    if (!identify) {
+      throw new Error("Unauthenticated")
+    }
+
+    const documents = await ctx.db.query("documents").collect()
+
+    return documents
+  }
+})
+
 export const create = mutation({
   args: {
     title: v.string(),
