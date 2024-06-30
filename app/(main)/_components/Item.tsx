85e9ff40a37cbcf8cc32eea8587b46519061a5a6
Sadjed Nemli <sajednemli39@gmail.com>
2024-06-30 11:34:03 +0100

    feat: Add create note functionality to Item component in navigation menu

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -1,10 +1,15 @@
 "use client"
 
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react"
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react"
+
+import { useMutation } from "convex/react"
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
 
 import { Id } from "@/convex/_generated/dataModel"
 import { cn } from "@/lib/utils"
 import { Skeleton } from "@/components/ui/skeleton"
+import { api } from "@/convex/_generated/api"
 
 interface ItemProps {
   id?: Id<"documents">,
@@ -31,11 +36,34 @@ export const Item = ({
   onExpand,
   expanded
 }: ItemProps) => {
+  const router = useRouter()
+  const create = useMutation(api.documents.create)
+
   const handleExpand = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
     event.stopPropagation()
     onExpand?.()
+  }
+
+  const onCreate = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation()
+    if (!id) return
+    const promise = create({ title: "Untitled", parentDocument: id })
+      .then((documentId) => {
+        if (!expanded) {
+          onExpand?.()
+        }
+        router.push(`/documents/${documentId}`)
+      })
+
+    toast.promise(promise), {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note"
+    }
   }
 
   const ChevronIcon = expanded ? ChevronDown : ChevronRight
@@ -78,6 +106,17 @@ export const Item = ({
           <span className="text-xs">ctrl
           </span>K
         </kbd>
+      )}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div
+            role="button"
+            onClick={onCreate}
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+          >
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </div>
       )}
     </div>
   )
